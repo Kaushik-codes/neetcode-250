193c1e948d8bac80c958d16c5712a7ea17c5a2ef
Kaushik Nag <kaushiknagofficial@gmail.com>
2026-09-09 13:38:10 +0530

    Add: longest-common-prefix - submission-0

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs) {
+        let x=0;
+        while(x<strs[0].length){
+            let ch = strs[0][x];
+
+            for(let i=1;i<strs.length;i++){
+                if(ch !== strs[i][x] || x===strs[i].length){
+                    return strs[i].substring(0,x);
+                }
+            }
+            x++;
+        }
+        return strs[0];
+    }
+}
